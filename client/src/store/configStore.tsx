5377b4d52f4b543a66fbe7d0af8ac2897e6c5943
Roger <candymask0712@gmail.com>
2022-02-13 17:05:02 +0900

    Feat: Add basic files

---
+++ client/src/store/configStore.tsx
@@ -0,0 +1,14 @@
+import listSlice from './listSlice';
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {
+    listSlice,
+  },
+});
+
+//state 타입을 export 해두는건데 나중에 쓸 데가 있음
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
