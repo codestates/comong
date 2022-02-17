719dee5dde45f8e25c63a707da72399006f51cb3
lvest <ookkid.1@gmail.com>
2022-02-17 11:06:34 +0900

    chore: api 주소 수정

--- client/src/apis/api/oauth.ts
+++ client/src/apis/api/oauth.ts
@@ -4,7 +4,7 @@ import { apiClient, apiOauthClient } from '..';
 export const postOauthNaver = async (code: string) => {
   // ! 네이버  - 개발 중 상태에서는 등록된 아이디만 로그인 가능
   try {
-    const response = await apiOauthClient.post(`/oauth/oauthnaver`, {
+    const response = await apiOauthClient.post(`/oauth/naver`, {
       authorizationCode: code,
     });
     console.log('naver', response);
@@ -14,7 +14,7 @@ export const postOauthNaver = async (code: string) => {
 
 export const postOauthKakao = async (code: string) => {
   try {
-    const response = await apiOauthClient.post(`/oauth/oauthkakao`, {
+    const response = await apiOauthClient.post(`/oauth/kakao`, {
       authorizationCode: code,
     });
     console.log('kakao', response);
@@ -24,7 +24,7 @@ export const postOauthKakao = async (code: string) => {
 
 export const postOauthGoogle = async (code: string) => {
   try {
-    const response = await apiOauthClient.post(`/oauth/oauthgoogle`, {
+    const response = await apiOauthClient.post(`/oauth/google`, {
       authorizationCode: code,
     });
     console.log('google', response);
