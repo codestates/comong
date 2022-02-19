055b14539ede747e89b523f69c975168c218d839
lvest <ookkid.1@gmail.com>
2022-02-20 03:54:54 +0900

    feat: 마이페이지 nav bar 반응형 구현(태블릿일 때만 토글 메뉴)

--- client/src/components/mypage/MypageAsideBar.tsx
+++ client/src/components/mypage/MypageAsideBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useAppSelector } from '../../redux/configStore.hooks';
 import BasicProfile from './BasicProfile';
@@ -10,16 +10,60 @@ const Wrapper = styled.div`
   width: 200px;
   border: 1px solid ${(props) => props.theme.colors.lightGrey};
   background-color: white;
+  z-index: 999;
+
+  @media only screen and (max-width: 1200px) {
+    position: absolute;
+  }
 `;
 
-function MypageAsideBar() {
+const ButtonWrapper = styled.div`
+  padding: 10px 10px 0 0;
+  text-align: end;
+`;
+
+const CloseBtn = styled.div``;
+
+// @media only screen and (max-width: 1200px) {
+// }
+// @media only screen and (max-width: 768px) {
+// }
+
+interface IMypageAsideBar {
+  setShowMenu?: (value: boolean) => void;
+}
+function MypageAsideBar({ setShowMenu }: IMypageAsideBar) {
+  const [width, setWidth] = useState(window.innerWidth);
+  const handleResize = () => setWidth(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const Desktop = () => {
+    return (
+      <Wrapper>
+        <BasicProfile></BasicProfile>
+        {role === 0 ? <NavUser></NavUser> : <NavSeller></NavSeller>}
+      </Wrapper>
+    );
+  };
+
+  const Tablet = () => {
+    return (
+      <Wrapper>
+        <ButtonWrapper>
+          <CloseBtn onClick={() => setShowMenu!(false)}>X</CloseBtn>
+        </ButtonWrapper>
+        <BasicProfile></BasicProfile>
+        {role === 0 ? <NavUser></NavUser> : <NavSeller></NavSeller>}
+      </Wrapper>
+    );
+  };
+
   const { role } = useAppSelector((state) => state.userSlice);
-  return (
-    <Wrapper>
-      <BasicProfile></BasicProfile>
-      {role === 0 ? <NavUser></NavUser> : <NavSeller></NavSeller>}
-    </Wrapper>
-  );
+  return <>{width > 1200 ? Desktop() : Tablet()}</>;
 }
 
 export default MypageAsideBar;
