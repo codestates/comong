54d9d6238fe9388de063f8e3360b168885634178
Hojin Jang <onewithtruth@gmail.com>
2022-02-21 14:05:06 +0900

    fix
    update variable name

--- server/src/modules/orders/orders.module.ts
+++ server/src/modules/orders/orders.module.ts
@@ -3,10 +3,12 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { MailerModule } from '../mailer/mailer.module';
 import { SocketModule } from '../socket/socket.module';
+import { AppGateway } from 'src/app.gateway';
+
 
 @Module({
   imports: [MailerModule, SocketModule],
   controllers: [OrdersController],
-  providers: [OrdersService]
+  providers: [OrdersService, AppGateway]
 })
 export class OrdersModule {}
