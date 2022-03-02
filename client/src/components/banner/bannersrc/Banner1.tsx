0e1338d0978210c90d5ecb96e969de3c1fa63c33
Roger <candymask0712@gmail.com>
2022-03-02 19:29:40 +0900

    Feat: 캐러샐 추가

---
+++ client/src/components/banner/bannersrc/Banner1.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: purple;
+  height: 100%;
+`;
+
+const Banner1 = () => {
+  return <Container>배너1번입니다</Container>;
+};
+
+export default Banner1;
